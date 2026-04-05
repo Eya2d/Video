c1a6b1cff4e456ba2c8615aa5885e058ca24e048
Eyad <eyadscript@gmail.com>
2026-04-05 05:56:21 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,8 +1,5 @@
-const videos = [
-  {
-    url: "https://ccdko80.com/videos/cmov/c-Movie-25-ar-480p.mp4"
-  },
-  {
-    url: "https://www.w3schools.com/html/movie.mp4"
-  }
+// data.js
+const videosData = [
+    { url: "https://ccdko80.com/videos/cmov/c-Movie-25-ar-480p.mp4" },
+    { url: "https://www.w3schools.com/html/movie.mp4" }
 ];
